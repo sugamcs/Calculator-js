fa1023e69e7af0c8babf9320e1f276e876890d42
sugamcs <sugambhargav1020@gmail.com>
2023-09-11 13:25:42 +0530

    feat: Create operate function which takes num1, num2 and operator as parameters

--- main.js
+++ main.js
@@ -11,5 +11,28 @@ function multiply(a,b){
 }
 
 function divide(a,b){
-    return a / b
+
+    return (a/b).toFixed(1)
 }
+
+let num1;
+let num2;
+let operator;
+
+
+function operate(num1,num2,operator){
+    if (operator === '+') {
+            return add(num1, num2);
+        } else if (operator === '-') {
+            return subtract(num1, num2);
+        } else if (operator === '*') {
+            return multiply(num1, num2);
+        } else if (operator === '/') {
+            return divide(num1, num2);
+        } else {
+            throw new Error(`Unsupported operator: ${operator}`);
+        }
+}
+
+const result = operate(5, 3, '/'); 
+console.log(result);
