cf3de143767e9a1e581e48baeb86540195b49c17
sugamcs <sugambhargav1020@gmail.com>
2023-09-14 23:31:21 +0530

    Store num1 after an operator has been clicked

--- main.js
+++ main.js
@@ -21,8 +21,13 @@ function divide(a,b){
 let num1;
 let num2;
 let operator;
+let displayText;
 
-// Operater
+
+
+
+// Operater Function:
+
 function operate(num1,num2,operator){
     if (operator === '+') {
             return add(num1, num2);
@@ -38,36 +43,49 @@ function operate(num1,num2,operator){
 }
 
 
-
-const digits = document.querySelectorAll('.digit');
+const digits = document.querySelectorAll('.digit')
 const equals = document.querySelector('#equals');
 const operations = document.querySelectorAll('.operation');
+const displaySelector = document.querySelector('.display')
 
 
 
-function populateDisplay(){
+function populateDisplay() {
     digits.forEach(digit => {
         digit.addEventListener('click', event => {
             const keyPressed = event.target;
-            const displayText = document.querySelector('.display');
-            let text1 = displayText.textContent += keyPressed.textContent;
-            if(operations.addEventListener('click',event =>{
-                text1 = num1;
-                const newDisplayText = displayText.textContent('')
-                let text2 = newDisplayText += event.target
+            let displayText = displaySelector.textContent += keyPressed.textContent
+            return displayText
+            })
+        })
+    }
 
-            
-            
-//progress
-            
+      
+populateDisplay()
 
 
-        
-        
-    
-            }))
-        }
+// store the first number and second number that are input into the calculator,
+// utilize the operator that the user selects, 
+// then operate() on the two numbers when user presses the “=” key.
+
+// i want to store num1 when an operator has been clicked
+
+function chadOperator() {
+    operations.forEach(operation =>{
+        operation.addEventListener('click', event => {
+
+            let text1 = event.target;
+            if (text1.textContent === '+'){
+                let num1;
+                num1 = displayText;
+                console.log(num1)
+
+            }
         })
     })
 }
-populateDisplay()
+           
+
+        
+console.log(chadOperator())
+
